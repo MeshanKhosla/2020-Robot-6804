03ee4b09a554dc0c67dee40b12dce9a51ffb54b7
Meshan Khosla <meshan02@gmail.com>
2020-01-24 18:30:14 -0800

    Limelight subsystem updates Created Shooter Subsystem Created Shoot command

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -14,14 +14,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
   
-  private NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-  NetworkTableEntry tx = table.getEntry("tx");
-  NetworkTableEntry ty = table.getEntry("ty");
-  NetworkTableEntry ta = table.getEntry("ta"); 
+  private NetworkTable table;
+  NetworkTableEntry tx;
+  NetworkTableEntry ty;
+  NetworkTableEntry ta;
+  NetworkTableEntry tv;
 
   
   public Limelight() {
- 
+    // Limelight network table
+    table = NetworkTableInstance.getDefault().getTable("limelight");
+    System.out.println("Limelight initialized");
+
+    // X offset
+    tx = table.getEntry("tx");
+    // Y offset
+    ty = table.getEntry("ty");
+    // Object area
+    ta = table.getEntry("ta");
+    // Whether there's a target detected
+    tv = table.getEntry("tv");
   }
 
   @Override
@@ -35,17 +47,51 @@ public class Limelight extends SubsystemBase {
     return tx;
   }
 
-
   public NetworkTableEntry getYOffset() {
     return ty;
   }
-
 
   public NetworkTableEntry getArea() {
     return ta;
   }
 
-  public void setPipeline(int pipeline) {
-    table.getEntry("pipeline").setNumber(pipeline);
+  public int getPipeline() {
+    return table.getEntry("pipeline");
   }
+
+
+   // Checks if there is a valid limelight target detected
+   public boolean hasTarget() {
+    if(tv == 0) {
+      return true;
+    } else if(tv == 1) {
+      return false;
+    }
+  }
+
+  // Up to 10 pipelines
+  public void setPipeline(int pipeline) {
+    if(pipeline >= 0 && pipeline <= 9) {
+      table.getEntry("pipeline").setNumber(pipeline);
+    }
+  }
+
+  // 0 = Vision processor
+  // 1 = Driver camera 
+  public void setCamMode(int mode) {
+    if (mode >= 0 && mode <= 1) {
+      table.getEntry("camMode").setNumber(mode);
+    }
+  }
+
+  // 0 = Current pipeline LED mode
+  // 1 = Force off
+  // 2 = Force blink
+  // 3 = Force on
+  public void setLedMode(int mode) {
+    if (mode >= 0 && mode <= 3) {
+      table.getEntry("ledMode").setNumber(mode);
+    }
+  }
+
 }
